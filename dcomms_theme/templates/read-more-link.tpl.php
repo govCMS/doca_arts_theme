bbb4a0d4472c745ace2e258bf0d4330eae19e438
Marc Groth <marc.groth.work@gmail.com>
2015-11-03 15:41:32 +1100

    DOC-29839 * template.helpers.inc: Added helper functions to: return the 'External source' field value and return whether an entity has an 'External source' filled in. * read-more-link.tpl.php: Added 'external-link' class when the 'external' value is TRUE. * Added 'External source' variable to the relevant preprocesses/templates.

--- dcomms_theme/templates/read-more-link.tpl.php
+++ dcomms_theme/templates/read-more-link.tpl.php
@@ -4,7 +4,7 @@
  * Template for read more link.
  */
 ?>
-<a class="read-more" href="<?php print $href; ?>">
+<a class="read-more<?php if ($external): ?> external-link<?php endif; ?>" href="<?php print $href; ?>" target="_parent">
   <?php print $text; ?>
   <svg focusable="false" xmlns='http://www.w3.org/2000/svg' height='15' version='1.1' viewBox='0 0 416 416' width='10' xml:space='preserve'><polygon points='160,115.4 180.7,96 352,256 180.7,416 160,396.7 310.5,256 '></polygon></svg>
 </a>

8db96d2795bb71eeb18c16df45044b86374bd686
Kurt Foster <kurt@salsadigital.com.au>
2015-11-06 08:45:41 +1100

    Adding the title field to the field tpl

--- dcomms_theme/templates/field--field_stackla_embed_para.tpl.php
+++ dcomms_theme/templates/field--field_stackla_embed_para.tpl.php
@@ -1,2 +1,5 @@
 <?php $stackla_arr = reset($items[0]['entity']['paragraphs_item']); ?>
+<?php if (!empty($stackla_arr['field_short_title']['#items'][0]['value'])): ?>
+<div id="stackla-title" class="stackla-title"><?php print $stackla_arr['field_short_title']['#items'][0]['value']; ?></div>
+<?php endif; ?>
 <div class="stacklafw" data-id="<?php print $stackla_arr['field_stackla_widget_id']['#items'][0]['value']; ?>" data-hash="<?php print $stackla_arr['field_stackla_hash_id']['#items'][0]['value']; ?>" data-ct="" data-alias="<?php print $stackla_arr['field_stackla_domain']['#items'][0]['value']; ?>" data-ttl="30" ></div>

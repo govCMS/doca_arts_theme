1d3d036b3e820c01454fe52fd2fee6feb4572e9b
Sonny Kieu <sonny@salsadigital.com.au>
2015-11-10 13:55:38 +1100

    [DOC-29824] Alter workbench_moderation_moderate_form to remove Archived state

--- dcomms_admin/template.php
+++ dcomms_admin/template.php
@@ -5,9 +5,19 @@
  * Conditionally remove the Archived state from publishing options if the node
  * has a currently published revision.
  */
-function dcomms_admin_form_node_form_alter(&$form, &$form_state, $form_id) {
-  $node = $form['#node'];
-  if (!empty($node->nid) && isset($node->workbench_moderation['published']->vid)) {
+function dcomms_admin_form_alter(&$form, &$form_state, $form_id) {
+  if (!in_array($form_id, array('node_form', 'workbench_moderation_moderate_form'))) {
+    return;
+  }
+
+  $node = NULL;
+  if ($form_id == 'node_form') {
+    $node = $form['#node'];
+  } else if ($form_id == 'workbench_moderation_moderate_form' && isset($form['node']['#value'])) {
+    $node = $form['node']['#value'];
+  }
+
+  if ($node && !empty($node->nid) && isset($node->workbench_moderation['published']->vid)) {
     unset($form['options']['workbench_moderation_state_new']['#options']['archive']);
   }
 }

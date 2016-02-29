f268cba233bfd86a07043a4eceff284c108ea902
Jason Guo <jason.guo@xing.net.au>
2016-02-29 16:00:41 +1100

    for the home page news grid, close the div after the 3rd item

--- dcomms_theme/templates/grid-stream/views-view-unformatted--whats-new-grid--block-1.tpl.php
+++ dcomms_theme/templates/grid-stream/views-view-unformatted--whats-new-grid--block-1.tpl.php
@@ -11,11 +11,11 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $delta => $row): ?>
-  <?php if ($delta == 1 || $delta == 4): ?>
+  <?php if ($delta == 1): ?>
     <div class="clearfix">
   <?php endif; ?>
   <?php print $row; ?>
-  <?php if ($delta == 1 || ($delta == (count($rows) - 1) && (($delta + 1) % 3) != 0)): ?>
+  <?php if ($delta == 2): ?>
     </div>
   <?php endif; ?>
 <?php endforeach; ?>

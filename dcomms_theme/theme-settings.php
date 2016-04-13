230ca4fea4b6508721fab90bb94490d7261ecea9
Jason Guo <jason.guo@finance.gov.au>
2016-04-13 14:06:17 +1000

    WEB-13: provide an Views RSS template to replace the user name with a desired value set in theme

--- dcomms_theme/theme-settings.php
+++ dcomms_theme/theme-settings.php
@@ -15,4 +15,10 @@ function dcomms_theme_form_system_theme_settings_alter(&$form, $form_state) {
     '#default_value' => theme_get_setting('cookie_textarea'),
     '#description'   => t("This is the message that will appear in the cookie notification at the top of your site."),
   );
+  $form['rss_author'] = array(
+    '#type'          => 'textfield',
+    '#title'         => t('RSS Author'),
+    '#default_value' => theme_get_setting('rss_author'),
+    '#description'   => t('Set author name to appear in the dc:creator field in RSS feeds.'),
+  );
 }

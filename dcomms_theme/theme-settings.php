6d773d0cd920b4a4b620831584baa0e64e56aaed
Joseph Zhao <joseph.zhao@xing.net.au>
2016-04-20 17:27:54 +1000

    [WEB-4] site pages feedback settings

--- dcomms_theme/theme-settings.php
+++ dcomms_theme/theme-settings.php
@@ -10,16 +10,22 @@
  */
 function dcomms_theme_form_system_theme_settings_alter(&$form, $form_state) {
   $form['cookie_textarea'] = array(
-    '#type'          => 'textarea',
-    '#title'         => t('Cookie Message'),
+    '#type' => 'textarea',
+    '#title' => t('Cookie Message'),
     '#default_value' => theme_get_setting('cookie_textarea'),
-    '#description'   => t("This is the message that will appear in the cookie notification at the top of your site."),
+    '#description' => t("This is the message that will appear in the cookie notification at the top of your site."),
   );
   $form['rss_author'] = array(
-    '#type'          => 'textfield',
-    '#title'         => t('RSS Author'),
+    '#type' => 'textfield',
+    '#title' => t('RSS Author'),
     '#default_value' => theme_get_setting('rss_author'),
-    '#description'   => t('Set author name to appear in the dc:creator field in RSS feeds.'),
+    '#description' => t('Set author name to appear in the dc:creator field in RSS feeds.'),
+  );
+  // Site pages feedback settings.
+  $form['feedback']['settings'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Site Pages Feedback'),
+    '#description' => t("Site pages feedback settings."),
   );
   $form['external_link_popup'] = array(
     '#type' => 'fieldset',

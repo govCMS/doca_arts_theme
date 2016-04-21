387c42499654e9f7704625e03f242d36c35acdc0
Jason Guo <jason.guo@finance.gov.au>
2016-04-21 22:02:24 +1000

    WEB-10: added theme settings for page top messages

--- dcomms_theme/theme-settings.php
+++ dcomms_theme/theme-settings.php
@@ -30,19 +30,38 @@ function dcomms_theme_form_system_theme_settings_alter(&$form, $form_state) {
     '#title' => t('Theme settings'),
     '#group' => 'group_tabs',
   );
-
+  // Page top announcements
+  $form['page_top_announcement'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Announcements to appear at the top of selected pages'),
+  );
+  $form['page_top_announcement']['page_top_announcement_messages'] = array(
+    '#type' => 'textarea',
+    '#title' => t('Message to display'),
+    '#default_value' => theme_get_setting('page_top_announcement_messages'),
+    '#description' => t('Message to display in the announcement area.'),
+  );
+  $form['page_top_announcement']['page_top_announcement_paths'] = array(
+    '#type' => 'textarea',
+    '#title' => t('Pages the announcements are displayed'),
+    '#default_value' => theme_get_setting('page_top_announcement_paths'),
+    '#description' => t('Internal paths where the announcements are displayed. Enter one path per line.'),
+  );
+  // Cookie message
   $form['cookie_textarea'] = array(
     '#type' => 'textarea',
     '#title' => t('Cookie Message'),
     '#default_value' => theme_get_setting('cookie_textarea'),
     '#description' => t("This is the message that will appear in the cookie notification at the top of your site."),
   );
+  // RSS author
   $form['rss_author'] = array(
     '#type' => 'textfield',
     '#title' => t('RSS Author'),
     '#default_value' => theme_get_setting('rss_author'),
     '#description' => t('Set author name to appear in the dc:creator field in RSS feeds.'),
   );
+  // External link popup controls
   $form['external_link_popup'] = array(
     '#type' => 'fieldset',
     '#title' => t('External link popup'),

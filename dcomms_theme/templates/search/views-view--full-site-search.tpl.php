f4629c6f9e4e3c20c53572e0a55f6f18400624c9
Jason Guo <jason.guo@finance.gov.au>
2016-08-03 13:29:32 +1000

    web-8: search page styling tweaks

--- dcomms_theme/templates/search/views-view--full-site-search.tpl.php
+++ dcomms_theme/templates/search/views-view--full-site-search.tpl.php
@@ -63,7 +63,7 @@
   <?php endif; ?>
 
   <?php if ($exposed): ?>
-    <div class="view-filters" style="position: absolute;">
+    <div class="view-filters">
       <?php print $exposed; ?>
     </div>
   <?php endif; ?>
@@ -75,7 +75,7 @@
   <?php endif; ?>
 
   <?php if ($rows): ?>
-    <div class="view-count" style="margin-top: 240px;">
+    <div class="view-count">
       <p class="font__small">
         <?php echo t('Showing !count of !total results, most relevant first.', array('!count' => count($view->result), '!total' => $view->total_rows)); ?>
       </p>

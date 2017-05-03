716067c506fb4f8833320e9b788b10efd982c163
Joseph Zhao <joseph.zhao@xing.net.au>
2017-05-03 16:11:41 +1000

    Reset the theme git version

--- docroot/sites/all/themes/custom/dcomms_theme/doca_common/templates/spotlight/field--quick-item--three-column-spotlight--field-pbundle-image.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @file
- * Template implementation to correctly output title in 3 column grid.
- */
-?>
-<?php foreach ($items as $delta => $item): ?>
-  <div class="spacer--bottom min-height__icon"><?php print render($item); ?></div>
-<?php endforeach; ?>
--- docroot/sites/all/themes/custom/dcomms_theme/doca_common/templates/summary-cta-with-links/field--field-pbundle-title--summary-cta-with-links-alt.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @file
- * Template implementation to display the title of a CTA summary item.
- */
-?>
-<?php foreach ($items as $delta => $item):?>
-  <h2><?php print render($item); ?></h2>
-<?php endforeach; ?>

e954fcab0634e4120383fb6edcdfe67fbe73cdc6
Joseph Zhao <joseph.zhao@xing.net.au>
2017-05-09 14:58:13 +1000

    Squashed 'doca_base/' changes from 9b2d5ab..9dea8fd
    9dea8fd Update README
    519a2ae Update common theme namespace
    59d298f Add doca_admin theme into base and restructure the files
    
    git-subtree-dir: doca_base
    git-subtree-split: 9dea8fdb534e379dd4a51fa8878e6543c6e4db69

--- templates/spotlight/field--quick-item--three-column-spotlight--field-pbundle-title.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @file
- * Template implementation to correctly output title in 3 column grid.
- */
-?>
-<?php foreach ($items as $delta => $item): ?>
-  <div class="heading--5"><?php print doca_theme_trim(render($item), 50); ?></div>
-<?php endforeach; ?>

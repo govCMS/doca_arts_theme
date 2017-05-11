244731d68b08efba8afe949a07a49b1f80d3bee7
Joseph Zhao <joseph.zhao@xing.net.au>
2017-05-11 14:24:47 +1000

    Provides the site gulp methods

--- doca_theme/gulpfile.js
+++ doca_theme/gulpfile.js
@@ -1,29 +1,27 @@
 'use strict';
 
 var theme = __dirname + '/';
-
 var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var rubySass = require('gulp-ruby-sass');
 var runSequence = require('run-sequence');
 var del = require('del');
 var path = require('path');
-
-var plugins = require('gulp-load-plugins')();
-var rubySass = require('gulp-ruby-sass');
-
+var merge = require('merge-stream');
 var compass = require('compass-options').dirs({'config': theme + 'config.rb'});
 var mainBowerFiles = require('main-bower-files');
 
 // Lint JavaScript.
 gulp.task('lint:js', function () {
-  return gulp.src(theme + '/src/js/*.js')
-    .pipe(plugins.jshint())
-    .pipe(plugins.jshint.reporter('jshint-stylish'));
+  return gulp.src(theme + 'src/js/*.js')
+      .pipe(plugins.jshint())
+      .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
 
 // Lint Sass.
 gulp.task('lint:sass', function () {
   return gulp.src(theme + compass.sass + '/**/*.scss')
-    .pipe(plugins.scssLint({'bundleExec': true, 'maxBuffer': Infinity}));
+      .pipe(plugins.scssLint({'bundleExec': true, 'maxBuffer': Infinity}));
 });
 
 // Lint Sass and JavaScript.
@@ -33,53 +31,63 @@ gulp.task('lint', function (cb) {
 
 // Concat and minify JavaScript.
 gulp.task('scripts', function () {
-  return gulp.src([theme + '/src/sass/components/**/*.js'])
-    .pipe(plugins.concat('components.min.js'))
-    .pipe(plugins.uglify())
-    .pipe(gulp.dest(theme + '/dist/js'));
+  var site = gulp.src([theme + 'src/js/*.js'])
+      .pipe(plugins.concat('site.min.js'))
+      .pipe(plugins.uglify())
+      .pipe(gulp.dest(theme + 'dist/js'));
+
+  var components = gulp.src([theme + '/src/sass/components/**/*.js'])
+      .pipe(plugins.concat('components.min.js'))
+      .pipe(plugins.uglify())
+      .pipe(gulp.dest(theme + '/dist/js'));
+
+  return merge(site, components);
 });
 
 gulp.task('bower-scripts', function () {
   return gulp.src(mainBowerFiles({
-      includeDev: true,
-      paths: {
-        bowerDirectory: __dirname + '/bower_components',
-        bowerJson: __dirname + '/bower.json'
-      },
-      filter: '**/*.js'
-    }))
-    .pipe(plugins.concat('bower-components.min.js'))
-    .pipe(plugins.uglify())
-    .pipe(gulp.dest(theme + 'js'));
+    includeDev: true,
+    paths: {
+      bowerDirectory: __dirname + '/bower_components',
+      bowerJson: __dirname + '/bower.json'
+    },
+    filter: '**/*.js'
+  }))
+      .pipe(plugins.concat('bower-components.min.js'))
+      .pipe(plugins.uglify())
+      .pipe(gulp.dest(theme + 'dist/js'));
 });
 
 gulp.task('bower-css', function () {
   return gulp.src(mainBowerFiles({
-      includeDev: true,
-      paths: {
-        bowerDirectory: __dirname + '/bower_components',
-        bowerJson: __dirname + '/bower.json'
-      },
-      filter: '**/*.css'
-    }))
-    .pipe(plugins.concat('bower-components.css'))
-    .pipe(gulp.dest(theme + 'css'));
+    includeDev: true,
+    paths: {
+      bowerDirectory: __dirname + '/bower_components',
+      bowerJson: __dirname + '/bower.json'
+    },
+    filter: '**/*.css'
+  }))
+      .pipe(plugins.concat('bower-components.css'))
+      .pipe(gulp.dest(theme + 'dist/css'));
 });
 
+// Bower components.
 gulp.task('bower', ['bower-scripts', 'bower-css']);
 
+// Use this if the current theme is not using docs_common as base theme.
 gulp.task('modernizr', function () {
   gulp.src(theme + './src/sass/components/**/*.{js,scss}')
-    .pipe(plugins.modernizr("modernizr.min.js", {
-      "options": [
-        "setClasses"
-      ]
-    }))
-    .pipe(plugins.uglify())
-    .pipe(gulp.dest("js/"))
+      .pipe(plugins.modernizr("modernizr.min.js", {
+        "options": [
+          "setClasses"
+        ]
+      }))
+      .pipe(plugins.uglify())
+      .pipe(gulp.dest(theme + "/dist/js/"))
 });
 
 process.chdir(theme);
+
 gulp.task('sass:development', function () {
   return rubySass(theme + 'src/sass/', {
     compass: true,
@@ -87,12 +95,12 @@ gulp.task('sass:development', function () {
     sourcemap: true,
     style: 'expanded'
   })
-    .on('error', function (err) {
-      console.error('Error!', err.message);
-    })
-    .pipe(plugins.pixrem())
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest('css'));
+      .on('error', function (err) {
+        console.error('Error!', err.message);
+      })
+      .pipe(plugins.pixrem())
+      .pipe(plugins.sourcemaps.write())
+      .pipe(gulp.dest('css'));
 });
 
 gulp.task('sass:production', function () {
@@ -101,17 +109,18 @@ gulp.task('sass:production', function () {
     bundleExec: true,
     style: 'compressed'
   })
-    .on('error', function (err) {
-      console.error('Error!', err.message);
-    })
-    .pipe(plugins.pixrem())
-    .pipe(gulp.dest('css'));
+      .on('error', function (err) {
+        console.error('Error!', err.message);
+      })
+      .pipe(plugins.pixrem())
+      .pipe(gulp.dest('css'));
 });
 
 // Styles
 gulp.task('watch', ['modernizr', 'sass:development', 'lint'], function () {
   gulp.watch(theme + 'src/sass/**/*.scss', ['modernizr', 'sass:development', 'lint']);
   gulp.watch(theme + 'src/sass/**/*.js', ['scripts']);
+  gulp.watch(theme + 'src/js/*.js', ['scripts']);
 });
 
 // Clean CSS directory.
@@ -122,10 +131,10 @@ gulp.task('clean', ['clean:css']);
 
 // Production build of front-end.
 gulp.task('build', ['clean', 'sass:production', 'scripts', 'modernizr'],
-  function (cb) {
-    // Run linting last, otherwise its output gets lost.
-    runSequence(['lint'], cb);
-  });
+    function (cb) {
+      // Run linting last, otherwise its output gets lost.
+      runSequence(['lint'], cb);
+    });
 
 // The default task.
 gulp.task('default', ['build']);

761af0517dfde6824f4208d65a5e764e7026240d
Joseph Zhao <joseph.zhao@xing.net.au>
2017-06-05 18:20:30 +1000

    Relocate block preprocess function in base theme

--- doca_common/template.php
+++ doca_common/template.php
@@ -140,40 +140,6 @@ function _doca_common_related_content($node) {
 }
 
 /**
- * Implements hook_preprocess_block().
- */
-function doca_common_preprocess_block(&$variables) {
-  // Theming various blocks.
-  switch ($variables['block_html_id']) {
-    case 'block-system-main-menu':
-      $variables['classes_array'][] = 'header-menu';
-      $variables['title_attributes_array']['class'] = ['element-invisible'];
-      break;
-
-    case 'block-menu-menu-footer-menu':
-      $variables['classes_array'][] = 'layout-centered';
-      $variables['classes_array'][] = 'clearfix';
-      $variables['title_attributes_array']['class'] = ['element-invisible'];
-      break;
-
-    case 'block-menu-menu-footer-sub-menu':
-      $variables['classes_array'][] = 'layout-centered';
-      $variables['classes_array'][] = 'clearfix';
-      $variables['title_attributes_array']['class'] = ['element-invisible'];
-      break;
-  }
-
-  // Block template per bean type.
-  if ($variables['block']->module === 'bean') {
-    $beans = $variables['elements']['bean'];
-    $bean_keys = element_children($beans);
-    $bean = $beans[reset($bean_keys)];
-    // Add template suggestions for bean types.
-    $variables['theme_hook_suggestions'][] = 'block__bean__' . $bean['#bundle'];
-  }
-}
-
-/**
  * Returns HTML for a menu with a heading and wrapper.
  */
 function _doca_common_block_render($module, $delta) {

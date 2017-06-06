fbd2c73f4dbbc58ce523e72807aae5bd2acd0f8b
Joseph Zhao <joseph.zhao@xing.net.au>
2017-06-06 15:01:35 +1000

    Update base theme style

--- doca_common/config.rb
+++ doca_common/config.rb
@@ -1,12 +1,3 @@
-#
-# This file is only needed for Compass/Sass integration. If you are not using
-# Compass, you may safely ignore or delete this file.
-#
-# If you'd like to learn more about Sass and Compass, see the sass/README.txt
-# file for more information.
-#
-
-
 # Require any additional compass plugins installed on your system.
 require 'breakpoint'
 require 'sass-globbing'

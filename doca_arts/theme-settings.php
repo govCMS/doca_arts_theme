0dc2799c4fafba8d65db9393974260dc65cb4745
Joseph Zhao <joseph.zhao@xing.net.au>
2017-07-12 15:00:54 +1000

    Clean up duplicated functions in theme

--- doca_arts/theme-settings.php
+++ doca_arts/theme-settings.php
@@ -4,3 +4,10 @@
  * @file
  * Theme settings.
  */
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function doca_theme_form_system_theme_settings_alter(&$form, $form_state) {
+  // Placeholder for custom theme settings form.
+}
